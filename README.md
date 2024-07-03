5957420004847659a9b536bd71d9bd9e616a9756
baran <bbrnylcn@gmail.com>
2024-07-03 16:33:13 +0300

    creating the project repostory

---
+++ README.md
@@ -0,0 +1,32 @@
+# Vehicle Segmentation and Classification
+This project focuses on segmenting and classifying images of vehicles using a deep learning approach with TensorFlow and Keras. The dataset consists of various vehicle images, and the model is trained to classify these images into different categories.
+
+# Project Overview
+This project utilizes a convolutional neural network (CNN) built on a pre-trained MobileNetV3 model to classify vehicle images. The project includes data preprocessing, model training, and evaluation steps, and it visualizes the results of the model's performance.
+
+# Dataset
+The dataset used in this project consists of images of vehicles, which are organized in subfolders representing different classes. The images are loaded and processed using the ImageDataGenerator class from Keras.
+![image](https://github.com/baranylcn/deneme/assets/98966968/ed224b42-6fd8-4f1e-b42d-62200f4e8b9f)
+
+
+# Model Architecture
+The model is built on the pre-trained MobileNetV3 architecture with additional dense layers for classification. The architecture includes:
+
+- Input resizing and rescaling layers
+- Data augmentation layers
+- MobileNetV3 base model
+- Additional dense layers with dropout for regularization
+- Output layer with softmax activation
+£ Training the Model
+The model is trained with the following parameters:
+- Optimizer: Adam
+- Loss: Categorical Crossentropy
+- Metrics: Accuracy, Precision, Recall
+- Epochs: 20
+- Batch size: 32
+
+# Results
+The model's performance is evaluated on a separate test dataset. The results include test loss and accuracy, which are printed after training. Additionally, training and validation accuracy/loss plots are generated to visualize the model's performance over epochs.
+- Test Accuracy: 91.54%
+![image](https://github.com/baranylcn/deneme/assets/98966968/247a5951-c92f-420c-bdbc-396e080ee8b0)
+
---
+++ app.py
@@ -0,0 +1,197 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+from tensorflow.keras import layers, Model
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.optimizers import Adam
+from pathlib import Path
+import os
+import warnings
+warnings.filterwarnings("ignore")
+
+
+BATCH_SIZE = 32
+IMAGE_SIZE = (300, 300)
+
+dataset = r"C:\Users\Huawei\OneDrive\Masaüstü\work\vehicle_segmentation\vehicle_segmentation2"
+image_dir = Path(dataset)
+
+# Get filepaths and labels
+filepaths = list(image_dir.glob(r'**/*.JPG')) + list(image_dir.glob(r'**/*.jpg')) + list(image_dir.glob(r'**/*.png'))  + list(image_dir.glob(r'**/*.jpeg')) + list(image_dir.glob(r'**/*.gif'))
+labels = list(map(lambda x: os.path.split(os.path.split(x)[0])[1], filepaths))
+
+filepaths = pd.Series(filepaths, name='Filepath').astype(str)
+labels = pd.Series(labels, name='Label')
+
+# Concatenate filepaths and labels
+image_df = pd.concat([filepaths, labels], axis=1)
+
+
+# Visualizing images from the dataset
+## Display 16 picture of the dataset with their labels
+random_index = np.random.randint(0, len(image_df), 16)
+fig, axes = plt.subplots(nrows=4, ncols=4, figsize=(10, 10),
+                        subplot_kw={'xticks': [], 'yticks': []})
+for i, ax in enumerate(axes.flat):
+    ax.imshow(plt.imread(image_df.Filepath[random_index[i]]))
+    ax.set_title(image_df.Label[random_index[i]])
+plt.tight_layout()
+plt.show(block=True)
+
+
+# Data Preprocessing
+## Separate in train and test data
+train_df, test_df = train_test_split(image_df, test_size=0.2, shuffle=True, random_state=42)
+
+train_generator = tf.keras.preprocessing.image.ImageDataGenerator(
+    preprocessing_function=tf.keras.applications.mobilenet_v2.preprocess_input,
+    validation_split=0.2
+)
+
+test_generator = tf.keras.preprocessing.image.ImageDataGenerator(
+    preprocessing_function=tf.keras.applications.mobilenet_v2.preprocess_input
+)
+
+train_images = train_generator.flow_from_dataframe(
+    dataframe=train_df,
+    x_col='Filepath',
+    y_col='Label',
+    target_size=(224, 224),
+    color_mode='rgb',
+    class_mode='categorical',
+    batch_size=32,
+    shuffle=True,
+    seed=42,
+    subset='training'
+)
+
+val_images = train_generator.flow_from_dataframe(
+    dataframe=train_df,
+    x_col='Filepath',
+    y_col='Label',
+    target_size=(224, 224),
+    color_mode='rgb',
+    class_mode='categorical',
+    batch_size=32,
+    shuffle=True,
+    seed=42,
+    subset='validation'
+)
+
+test_images = test_generator.flow_from_dataframe(
+    dataframe=test_df,
+    x_col='Filepath',
+    y_col='Label',
+    target_size=(224, 224),
+    color_mode='rgb',
+    class_mode='categorical',
+    batch_size=32,
+    shuffle=False
+)
+
+# Resize Layer
+resize_and_rescale = tf.keras.Sequential([
+    layers.Resizing(224, 224),
+    layers.Rescaling(1./255),
+    layers.RandomFlip("horizontal"),
+    layers.RandomRotation(0.1),
+    layers.RandomZoom(0.1),
+    layers.RandomContrast(0.1),
+])
+
+
+# Training the model
+## Load the pretained model
+pretrained_model = tf.keras.applications.MobileNetV3Large(
+    input_shape=(224, 224, 3),
+    include_top=False,
+    weights='imagenet',
+    pooling='max'
+)
+pretrained_model.trainable = False
+
+# Create checkpoint callback
+checkpoint_path = "vehicle_classification_model_checkpoint.weights.h5"
+checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path,
+                                      save_weights_only=True,
+                                      monitor="val_accuracy",
+                                      save_best_only=True)
+
+# Training the model
+inputs = pretrained_model.input
+x = resize_and_rescale(inputs)
+
+x = Dense(128, activation='relu')(pretrained_model.output)
+x = Dropout(0.2)(x)
+x = Dense(128, activation='relu')(x)
+x = Dropout(0.2)(x)
+outputs = Dense(3, activation='softmax')(x)
+model = Model(inputs=inputs, outputs=outputs)
+
+model.compile(
+    optimizer=Adam(),
+    loss='categorical_crossentropy',
+    metrics=['accuracy', tf.keras.metrics.Precision(), tf.keras.metrics.Recall()]
+)
+
+history = model.fit(
+    train_images,
+    steps_per_epoch=len(train_images),
+    validation_data=val_images,
+    validation_steps=len(val_images),
+    epochs=30,
+    callbacks=[checkpoint_callback]
+)
+
+results = model.evaluate(test_images, verbose=0)
+print("Test Loss: {:.5f}".format(results[0]))
+print("Test Accuracy: {:.2f}%".format(results[1] * 100))
+"""
+Test Loss: 0.24113
+Test Accuracy: 91.54%
+"""
+
+
+accuracy = history.history['accuracy']
+val_accuracy = history.history['val_accuracy']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(len(accuracy))
+plt.plot(epochs, accuracy, 'bo', label='Training accuracy')
+plt.plot(epochs, val_accuracy, 'b', label='Validation accuracy')
+plt.title('Training and validation accuracy')
+plt.legend()
+plt.figure()
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+plt.show(block=True)
+
+# Predict the label of the test_images
+pred = model.predict(test_images)
+pred = np.argmax(pred,axis=1)
+
+# Map the label
+labels = (train_images.class_indices)
+labels = dict((v,k) for k,v in labels.items())
+pred = [labels[k] for k in pred]
+
+
+# Display 25 random pictures from the dataset with their labels
+random_index = np.random.randint(0, len(test_df) - 1, 15)
+fig, axes = plt.subplots(nrows=3, ncols=5, figsize=(25, 15),
+                        subplot_kw={'xticks': [], 'yticks': []})
+for i, ax in enumerate(axes.flat):
+    ax.imshow(plt.imread(test_df.Filepath.iloc[random_index[i]]))
+    if test_df.Label.iloc[random_index[i]] == pred[random_index[i]]:
+        color = "green"
+    else:
+        color = "red"
+    ax.set_title(f"True: {test_df.Label.iloc[random_index[i]]}\nPredicted: {pred[random_index[i]]}", color=color)
+plt.show(block=True)
+plt.tight_layout()
+
